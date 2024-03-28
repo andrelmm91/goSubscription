15b59996946d9566b76bd1e0afc7296877714755
andrelmm91 <andre.lmm91@gmail.com>
2024-03-28 20:53:20 +0100

    add http server, and templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,34 +4,69 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"sync"
 	"time"
 
+	"github.com/alexedwards/scs/redisstore"
+	"github.com/alexedwards/scs/v2"
+	"github.com/gomodule/redigo/redis"
 	_ "github.com/jackc/pgconn"
 	_ "github.com/jackc/pgx/v4"
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-// var webPort = 80
+var webPort = 80
 
 func main() {
 	// connect to database
 	db := initDB()
-	fmt.Println(db)
-	db.Ping()
 
 	// create sessions
+	session := initSession()
+
+	// create loggers
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	// create channels
 
 	// create waitgroups
+	wg := sync.WaitGroup{}
 
 	// set up the application config
+	app := Config{
+		Session: session,
+		DB: db,
+		InfoLog: infoLog,
+		ErrorLog: errorLog,
+		Wait: &wg,
+	}
 
 	// set up mails
 
 	// listen for web connection
+	app.serve()
 }
+
+func (app *Config) serve() {
+	// start http server
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	app.InfoLog.Println("Starting web server...")
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+/////////////////////////////////////////////
+// Connecting to DB
+/////////////////////////////////////////////
 
 func initDB() *sql.DB {
 	conn, err := connectToDB()
@@ -82,4 +117,31 @@ func openDB(dsn string) (*sql.DB, error) {
 	}
 
 	return db, nil
+}
+
+/////////////////////////////////////////////
+// Session
+/////////////////////////////////////////////
+
+func initSession() *scs.SessionManager {
+	// set up Session
+	session := scs.New()
+	session.Store = redisstore.New(initRedis())
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = true
+
+	return session
+}
+
+func initRedis() *redis.Pool {
+	redisPool := &redis.Pool{
+		MaxIdle: 10,
+		Dial: func () (redis.Conn, error) {
+			return redis.Dial("tcp", os.Getenv("REDIS"))
+		},
+	}
+
+	return redisPool
 }
